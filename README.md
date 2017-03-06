95822ebe418305fd931041952f28e1ffec1172b3
Edward Hu <hues@usc.edu>
2017-03-05 16:00:22 -0800

    tweaked hyperparameters of linear classfier

--- README.md
+++ README.md
@@ -4,10 +4,21 @@ I will build figures, classifiers, and other fun data related things around this
 I will be using __Tensorflow__, __Numpy__, and __Pandas__.
 # Pandas / matplotlib warmup
 Located in `stats.py`, shows trending dogs and information about their owners
-
+![Chart](images/stats.png)
 # Linear classifier
 Classifies the gender of the owner based on dog features.
-
+```
+accuracy: 0.686246
+accuracy/baseline_label_mean: 0.313754
+accuracy/threshold_0.500000_mean: 0.686246
+auc: 0.5
+global_step: 150
+labels/actual_label_mean: 0.313754
+labels/prediction_mean: 2.76343e-07
+loss: 4.78862
+precision/positive_threshold_0.500000_mean: 0.0
+recall/positive_threshold_0.500000_mean: 0.0
+```
 # Neural Network classifier
 Work in progress
 
--- linearclassifier.py
+++ linearclassifier.py
@@ -3,12 +3,12 @@ import pandas as pd
 import tempfile
 COLUMNS = ['HALTER_ID','ALTER','GESCHLECHT','STADTKREIS','STADTQUARTIER','RASSE1','RASSE1_MISCHLING','RASSE2',
            'RASSE2_MISCHLING','RASSENTYP','GEBURTSJAHR_HUND','GESCHLECHT_HUND','HUNDEFARBE']
-CONTINUOUS_COLUMNS = ['AVG_AGE', 'GEBURTSJAHR_HUND', 'STADTQUARTIER', 'STADTKREIS']
+CONTINUOUS_COLUMNS = ['AVG_AGE', 'GEBURTSJAHR_HUND']
 #'STADTQUARTIER'
-CATEGORICAL_COLUMNS = [ 'RASSE1',  'GESCHLECHT_HUND', 'HUNDEFARBE']
+CATEGORICAL_COLUMNS = [ 'RASSE1',  'GESCHLECHT_HUND', 'HUNDEFARBE', 'STADTQUARTIER', 'STADTKREIS']
 LABEL_COLUMN = 'GESCHLECHT'
 def create_avg_age_col(df):
-    print('Creating Minage Maxage columns')
+    # print('Creating Minage Maxage columns')
     df = df[pd.notnull(df['ALTER'])]
     # create age_min and age_max columns
     age_series = df['ALTER']
@@ -38,16 +38,17 @@ def build_estimator():
     dog_breed =  tf.contrib.layers.sparse_column_with_hash_bucket("RASSE1", hash_bucket_size=100)
     dog_gender =  tf.contrib.layers.sparse_column_with_hash_bucket("GESCHLECHT_HUND", hash_bucket_size=100)
     dog_color =  tf.contrib.layers.sparse_column_with_hash_bucket("HUNDEFARBE", hash_bucket_size=100)
+    city_quarter = tf.contrib.layers.sparse_column_with_hash_bucket("STADTQUARTIER", hash_bucket_size=400)
+    city_district = tf.contrib.layers.sparse_column_with_hash_bucket("STADTKREIS", hash_bucket_size=400)
 
 
-    city = tf.contrib.layers.real_valued_column("STADTKREIS")
-    city_quarter = tf.contrib.layers.real_valued_column("STADTQUARTIER")
+    # city = tf.contrib.layers.real_valued_column("STADTKREIS")
     avg_age = tf.contrib.layers.real_valued_column("AVG_AGE")
     dog_byear = tf.contrib.layers.real_valued_column("GEBURTSJAHR_HUND")
 
 
     model_dir = tempfile.mkdtemp()
-    m = tf.contrib.learn.LinearClassifier(model_dir=model_dir,feature_columns=[city,city_quarter,dog_breed,dog_gender,
+    m = tf.contrib.learn.LinearClassifier(model_dir=model_dir,feature_columns=[city_district, city_quarter, dog_breed,dog_gender,
     dog_color,avg_age, dog_byear])
     return m
 def convert_label_to_int(x):
@@ -56,17 +57,23 @@ def convert_label_to_int(x):
     else:
         return 1
 def main():
-    df_train = pd.read_csv('data.csv')
-    df_test = pd.read_csv('data2.csv')
+    df = pd.read_csv('data.csv')
+    df['STADTQUARTIER'] = df['STADTQUARTIER'].astype(str)
+    df['STADTKREIS'] = df['STADTKREIS'].astype(str)
+    # print(df['STADTKREIS'])
+    df_train=df.sample(frac=0.8,random_state=200)
+    df_test=df.drop(df_train.index)
     df_train[LABEL_COLUMN] =  (df_train[LABEL_COLUMN].apply(convert_label_to_int)).astype(int)
     df_test[LABEL_COLUMN] =  (df_test[LABEL_COLUMN].apply(convert_label_to_int)).astype(int)
+    # print(df_train[LABEL_COLUMN])
     df_train = create_avg_age_col(df_train)
     df_test = create_avg_age_col(df_test)
-    # print(df_train['STADTKREIS'].dtype)
+    # # print(df_train['STADTKREIS'].dtype)
     df_train.dropna(subset=CONTINUOUS_COLUMNS + CATEGORICAL_COLUMNS + [LABEL_COLUMN])
     df_test.dropna(subset=CONTINUOUS_COLUMNS + CATEGORICAL_COLUMNS + [LABEL_COLUMN])
+    # print(df_test)
     m = build_estimator()
-    m.fit(input_fn=lambda: input_fn(df_train), steps=200)
+    m.fit(input_fn=lambda: input_fn(df_train), steps=150)
     results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
     for key in sorted(results):
         print ("%s: %s" % (key, results[key]))
